4b952932ddc0d33645fe1cc58e1bf59e57119444
you88311 <you88311@naver.com>
2022-06-28 23:37:13 +0900

    no message

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,7 @@
+public class DoublyLinkedList<T> {
+
+    private Node<T> prime = new Node<>();
+    int length = 0;
+
+
+}
---
+++ src/Node.java
@@ -0,0 +1,5 @@
+public class Node<T> {
+    Node<T> left = null;
+    Node<T> right = null;
+    T data = null;
+}
