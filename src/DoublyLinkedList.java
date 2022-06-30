7b3096b007e96ed8bd2625f92d2f69f228b37896
you88311 <you88311@naver.com>
2022-06-30 17:28:23 +0900

    no message

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class DoublyLinkedList<T> {
 
     private Node<T> head = null;
@@ -31,11 +33,33 @@ public class DoublyLinkedList<T> {
         return foundNode;
     }
 
-    public void add(int addIndex, T data){
+    public int indexOf(T data) {
+        int foundIndex = 0;
+        Node<T> foundNode = this.head;
+
+        while (foundNode != null) {
+            if (foundNode.data == null && data == null) {
+                //값이 null인 경우 equals()함수 사용 불가하기에 별도 구현
+                return foundIndex;
+            } else if (data != null && data.equals(foundNode.data)) {
+                return foundIndex;
+            }
+            foundNode = foundNode.right;
+            foundIndex++;
+        }
+        //못 찾은 경우
+        return -1;
+    }
+
+    public boolean contains(T data) {
+        return indexOf(data) != -1;
+    }
+
+    public void add(int addIndex, T data) {
         Node<T> addNode = new Node<>();
         addNode.data = data;
 
-        if(this.length ==0){
+        if (this.length == 0) {
             //최초 노드 삽입인 경우
             this.head = addNode;
             this.tail = addNode;
@@ -49,10 +73,11 @@ public class DoublyLinkedList<T> {
             addNode.left = this.tail;
             this.tail.right = addNode;
             this.tail = addNode;
-        }else{
+        } else {
             //중간에 노드 삽입인 경우
-            Node<T> leftNode = findNode(addIndex);
-            Node<T> rightNode = leftNode.right;
+
+            Node<T> rightNode = findNode(addIndex);
+            Node<T> leftNode = rightNode.left;
 
             addNode.left = leftNode;
             addNode.right = rightNode;
@@ -62,4 +87,76 @@ public class DoublyLinkedList<T> {
         this.length++;
     }
 
+    public void addFirst(T data) {
+        add(0, data);
+    }
+
+    public void addLast(T data) {
+        add(this.length, data);
+    }
+
+    public void removeNode(Node<T> removingNode) {
+        Node<T> leftNode = removingNode.left;
+        Node<T> rightNode = removingNode.right;
+        if(isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if (this.length == 1) {
+            //노드가 한 개인 경우
+            this.head = null;
+            this.tail = null;
+        } else if (leftNode == null) {
+            //맨 앞 노드 삭제인 경우
+            rightNode.left = null;
+            this.head = rightNode;
+        } else if (rightNode == null) {
+            //맨 뒤 노드 삭제인 경우
+            leftNode.right = null;
+            this.tail = leftNode;
+        } else {
+            //중간 노드 삭제인 경우
+            leftNode.right = rightNode;
+            rightNode.left = leftNode;
+        }
+        removingNode.left = null;
+        removingNode.right = null;
+        this.length--;
+    }
+
+    public void remove(int removeIndex) {
+        removeNode(findNode(removeIndex));
+    }
+
+    public void removeFirst() {
+        remove(0);
+    }
+
+    public void removeLast() {
+        remove(length - 1);
+    }
+
+    public void removeAll() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void display() {
+        if (length == 0) {
+            System.out.println("비어있습니다.");
+            return;
+        }
+
+        Node<T> pointer = this.head;
+        while (pointer != null) {
+            System.out.printf(pointer.data + " ");
+            pointer = pointer.right;
+        }
+        System.out.println();
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<>();
+        doublyLinkedList.addFirst("A");
+        doublyLinkedList.addLast("B");
+        doublyLinkedList.addLast("D");
+        doublyLinkedList.addLast("E");
+        doublyLinkedList.add(2, "C");
+
+        doublyLinkedList.display();
+
+        System.out.println("값 C 갖는 노드의 index: " + doublyLinkedList.indexOf("C"));
+        System.out.println("List는 값 E를 값고 있다: " + doublyLinkedList.contains("E"));
+        System.out.println("List는 값 F를 값고 있다: " + doublyLinkedList.contains("F"));
+
+        doublyLinkedList.remove(1);
+        doublyLinkedList.display();
+
+        doublyLinkedList.removeFirst();
+        doublyLinkedList.display();
+
+        doublyLinkedList.removeLast();
+        doublyLinkedList.display();
+
+        doublyLinkedList.removeAll();
+        doublyLinkedList.display();
+
+//        doublyLinkedList.remove(0);
+//        Node<String> node = new Node<String>();
+//        doublyLinkedList.removeNode(node);
+          //없는 값을 삭제하려고 하기에 ArrayIndexOutOfBoundsException 발생시킴
+    }
+}
